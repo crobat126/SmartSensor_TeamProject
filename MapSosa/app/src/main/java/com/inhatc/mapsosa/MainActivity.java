2bbfa24f28f648ff9fbde54ffbca2b7adebba296
jseoup <crobat126@naver.com>
2021-05-08 16:47:31 +0900

    Accelerometer (가속도) 값만 표현

--- MapSosa/app/src/main/java/com/inhatc/mapsosa/MainActivity.java
+++ MapSosa/app/src/main/java/com/inhatc/mapsosa/MainActivity.java
@@ -2,13 +2,74 @@ package com.inhatc.mapsosa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager objSMG;                   // Object SensorManager
+
+    Sensor sensor_Accelerometer;            // Object Sensor Accelerometer
+
+    TextView objTV_X_Accelerometer;         // Accelerometer X Variable
+    TextView objTV_Y_Accelerometer;         // Accelerometer Y Variable
+    TextView objTV_Z_Accelerometer;         // Accelerometer Z Variable
+    TextView objTV_Total_Accelerometer;     // Total Accelerometer Variable
+
+    float total_Accelerometer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Object for access sensor device
+        objSMG = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensor_Accelerometer = objSMG.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        objTV_X_Accelerometer = (TextView) findViewById(R.id.txtX_Accelerometer);
+        objTV_Y_Accelerometer = (TextView) findViewById(R.id.txtY_Accelerometer);
+        objTV_Z_Accelerometer = (TextView) findViewById(R.id.txtZ_Accelerometer);
+        objTV_Total_Accelerometer = (TextView) findViewById(R.id.txtTotal_Accelerometer);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register listener for changing sensor value
+        objSMG.registerListener(this, sensor_Accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Release sensor listener
+        objSMG.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        switch (sensorEvent.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                objTV_X_Accelerometer.setText(("X : " + sensorEvent.values[0]));
+                objTV_Y_Accelerometer.setText(("Y : " + sensorEvent.values[1]));
+                objTV_Z_Accelerometer.setText(("Z : " + sensorEvent.values[2]));
+                total_Accelerometer = (float)sensorEvent.values[0] + (float)sensorEvent.values[1] + (float)sensorEvent.values[2];
+                objTV_Total_Accelerometer.setText(("Total : " + total_Accelerometer));
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // Call when sensor accuracy changed
     }
 }
